25b2ece46ef098981a47776a3c2abcd505ce71cf
gecicidegisken <hayallerimyaziyor@gmail.com>
2021-07-14 14:03:37 +0300

    input issues -1

--- script.js
+++ script.js
@@ -4,8 +4,13 @@ const btn = document.getElementById("addBtn");
 var leng = 0;
 // Create the new task
 function newTask() {
+  //Check Task Count
   if (leng > 9) {
     btn.addEventListener("click", alert("Enough tasks for today :)"));
+  }
+  //Check if input is not null
+  else if (input.value === "" || input.value === null) {
+    btn.addEventListener("click", alert("Task cannot be empty."));
   } else {
     var newTask = document.createElement("LI");
     var textnode = document.createTextNode(input.value);
@@ -20,6 +25,7 @@ function newTask() {
     deleteBtn.className = "deleteBtn";
     deleteBtn.appendChild(cross);
     newTask.appendChild(deleteBtn);
+    delBtn(deleteBtn, leng, newTask); //del btn funcs
     deleteBtn.addEventListener("click", function () {
       newTask.classList.toggle("delete");
       leng--;
@@ -33,11 +39,30 @@ function newTask() {
     newTask.appendChild(checkBox);
     checkBox.addEventListener("click", function () {
       newTask.classList.toggle("done");
+      checkBox.classList.toggle("checked");
     });
   }
 }
 
+//functions to add new task
 btn.addEventListener("click", newTask, false);
 
+function enter(e) {
+  if (e.type === "keypress") {
+    if (e.which == 13 || e.keycode == 13) {
+      newTask();
+    }
+  }
+}
+input.addEventListener("keypress", enter);
+
+//Delete Button Features
+function delBtn(button, length, task) {
+  button.addEventListener("mouseenter", function () {
+    task.style.backgroundColor = "#f56642";
+  });
+  button.addEventListener("mouseleave", function () {
+    task.style.backgroundColor = "white";
+  });
+}
 //events
-//Check Task Count
